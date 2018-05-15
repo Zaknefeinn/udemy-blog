7947c45b7b05d2e6b2972e0645d5218a58f25ab3
Eric <zaknefeinn@gmail.com>
2018-05-15 16:54:58 -0500

    end of work day

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {createPost} from ''
 
 class PostsNew extends Component {
-
   renderField(field) {
+    //field.meta.touched
+    //field.meta.error
+    const {
+      meta: { touched, error }
+    } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
@@ -15,14 +22,19 @@ class PostsNew extends Component {
           //onFocus = {field.input.onFocus}
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">{touched ? error : ''}</div>
       </div>
-    )
+    );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           //Pass a prop to renderField...
           label="Title For Post"
@@ -39,24 +51,29 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
-    )
+    );
   }
 }
-
 
 function validate(values) {
   const errors = {};
 
   //Validate the inputs from 'values'
   if (!values.title) {
-    errors.title = "Enter a title"
+    errors.title = 'Enter a title';
   }
   if (!values.categories) {
-    errors.categories = "Enter some categories"
+    errors.categories = 'Enter some categories';
   }
   if (!values.content) {
-    errors.content = "Enter some content please"
+    errors.content = 'Enter some content please';
   }
   //If errors is empty, the form is fine to submit
 
